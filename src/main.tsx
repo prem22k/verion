7d81d48c99a364f20c944633b7f7f917299d2ea8
prem22k <premsai224k@gmail.com>
2026-07-17 14:14:21 +0530

    feat(ui): add ibm plex typography and genuine simple-icons dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource-variable/ibm-plex-sans/wght.css'
+import '@fontsource/ibm-plex-mono/400.css'
 import { App } from './App'
 import { DemoTargetApp } from './DemoTargetApp'
 import './styles.css'
